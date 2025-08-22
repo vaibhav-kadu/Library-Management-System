35c70fa299a10373246876251f3e75a1fc98f2f9
vaibhav-kadu <vkdon02@gmail.com>
2025-08-22 22:39:48 +0530

    Add Student  &  Librarian

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 
 import { BrowserRouter ,Routes,Route } from 'react-router-dom';
 import AddStudents from './pages/students/addStudents';
-import ViewStudents from './pages/students/viewStudents';
 import Login from './pages/Login';
 import LibrarianDashboard from './pages/librarian/LibrarianDashboard';
 import ProfileAccountManagement from './pages/admin/ProfileAccountManagement';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import PrivateRoutes from './utils/PrivateRoutes';
 import RoleBaseRoutes from './utils/RoleBaseRoutes';
-import ViewAllBooks from './pages/ViewAllBooks';
+import ViewAllBooks from './pages/books/ViewAllBooks';
 import StudentDashboard from './pages/students/StudentDashboard';
 import AddLibrarian from './pages/librarian/AddLibrarian';
 import AddBook from './pages/books/AddBook';
+import StudentProfile from './pages/students/StudentProfile ';
+import AddCategory from './pages/category/AddCategory';
+import ViewCategories from './pages/category/ViewCategories';
 
 function App() {
 
@@ -41,6 +43,14 @@ function App() {
           </PrivateRoutes>
         }></Route>
 
+        <Route path='/addLibrarian' element={
+          <PrivateRoutes>
+            <RoleBaseRoutes requiredRole={['admin','librarian']}>
+              <AddStudents/>
+            </RoleBaseRoutes>
+          </PrivateRoutes>
+        }></Route>
+
         <Route path='/viewAllBooks' element={<ViewAllBooks/>}/>
 
         <Route path='/profileAccountManagement' element={<ProfileAccountManagement/>} />
@@ -49,7 +59,6 @@ function App() {
          <Route path='/viewCategory' element={<ViewCategories/>}/>
 
           <Route path='/addBook' element={<AddBook/>}/>
-         <Route path='/viewBook' element={<ViewBook/>}/>
 
 
 
