ed856a23ebf9643a11665dd9ebcf4f7532a02560
vaibhav-kadu <vkdon02@gmail.com>
2025-09-26 22:33:44 +0530

    Bug Fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import App from './App.jsx'
-import AuthContext from './context/authContext.jsx';
+import AuthContext from '../src/context/AuthContext.jsx';
 import Navbar from './components/Navbar.jsx';
 
 createRoot(document.getElementById('root')).render(
